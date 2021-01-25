b89c9e826a49167c11270a29e7e0b5c26bc1a9e2
garymverde <gary@solutionsbygary.com>
2021-01-25 11:51:49 -0800

    Messing around with adding some static content to the helloworld sample application

--- helloworld/src/main/scala/bootstrap/liftweb/Boot.scala
+++ helloworld/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -26,9 +26,10 @@ class Boot {
 
       // more complex because this menu allows anything in the
       // /static path to be visible
-      Menu(Loc("Static", Link(List("static"), true, "/static/index"), 
-	       "Static Content")))
-
+//      Menu(Loc("Static", Link(List("static"), true, "/static/index"), "Static Content")))
+      Menu.i("Dummy") / "static/dummy",       //This didn't work as expected.  A sub-menu here would be a good idea
+      Menu.i("Static") / "static" / **
+    )
     // set the sitemap.  Note if you don't want access control for
     // each page, just comment this line out.
     LiftRules.setSiteMap(SiteMap(entries:_*))
